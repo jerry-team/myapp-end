fb6d3fbbec15b391a7f3dca1de3d53340d618dfd
Jerry <719622388@qq.com>
2022-06-21 11:25:30 +0800

    XY v0.3

--- src/test/java/com/jerry/myappend/MyappEndApplicationTests.java
+++ src/test/java/com/jerry/myappend/MyappEndApplicationTests.java
@@ -1,13 +1,27 @@
 package com.jerry.myappend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class MyappEndApplicationTests {
 
+    @Autowired
+//    UserMapper tuserMapper;
+//    User user;
     @Test
     void contextLoads() {
+//        user.setPassword("2711055a");
+//        user.setUsername("719622388");
+//        user.setNickname("鲜钰");
+//        user.setState(1);
+//        user.setCreateTime(new Date());
+//        user.setUpdateTime(new Date());
+//        tuserMapper.insert(user);
     }
 
 }
